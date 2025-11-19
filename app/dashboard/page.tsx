28b87427ed68de548237d9a64c82b3336569d596
felimili <arieloscar2000@gmail.com>
2025-11-19 16:25:11 -0300

    ok

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -20,52 +20,54 @@ export default async function DashboardPage() {
 
 
   return (
-    <Tabs defaultValue="informes" className="w-full">
-      <TabsList className="w-full">
-        <TabsTrigger value="informes">Informes</TabsTrigger>
-        <TabsTrigger value="tickets">Tickets</TabsTrigger>
-      </TabsList>
+    <div className="container mx-auto p-1">
+      <Tabs defaultValue="informes" className="w-full">
+        <TabsList className="w-full">
+          <TabsTrigger value="informes">Informes</TabsTrigger>
+          <TabsTrigger value="tickets">Tickets</TabsTrigger>
+        </TabsList>
 
-      {/* ----- TAB PRINCIPAL: INFORMES ----- */}
-      <TabsContent value="informes" className="mt-4">
+        {/* ----- TAB PRINCIPAL: INFORMES ----- */}
+        <TabsContent value="informes" className="mt-4">
 
-        {/* Tabs secundarios */}
-        <Tabs defaultValue="inf1" className="w-full">
-          <TabsList>
-            <TabsTrigger value="inf1">Informe A</TabsTrigger>
-            <TabsTrigger value="inf2">Informe B</TabsTrigger>
-            <TabsTrigger value="inf3">Informe C</TabsTrigger>
-          </TabsList>
+          {/* Tabs secundarios */}
+          <Tabs defaultValue="inf1" className="w-full">
+            <TabsList>
+              <TabsTrigger value="inf1">Informe A</TabsTrigger>
+              <TabsTrigger value="inf2">Informe B</TabsTrigger>
+              <TabsTrigger value="inf3">Informe C</TabsTrigger>
+            </TabsList>
 
-          <TabsContent value="inf1" >
-            <iframe
-              src={informe1 || "https://assistcargo.com"}
-              className="border rounded w-full min-h-[700px]"
-            />
-          </TabsContent>
+            <TabsContent value="inf1" >
+              <iframe
+                src={informe1 || "https://assistcargo.com"}
+                className="border rounded w-full min-h-[700px]"
+              />
+            </TabsContent>
 
-          <TabsContent value="inf2" className="min-h-screen ">
-            <iframe
-              src={informe2 || "https://assistcargo.com"}
-              className="border rounded w-full min-h-[700px]"
-            />
-          </TabsContent>
+            <TabsContent value="inf2" className="min-h-screen ">
+              <iframe
+                src={informe2 || "https://assistcargo.com"}
+                className="border rounded w-full min-h-[700px]"
+              />
+            </TabsContent>
 
-          <TabsContent value="inf3" className="min-h-screen ">
-            <iframe
-              src={informe3 || "https://assistcargo.com"}
-              className="border rounded w-full min-h-[700px]"
-            />
-          </TabsContent>
+            <TabsContent value="inf3" className="min-h-screen ">
+              <iframe
+                src={informe3 || "https://assistcargo.com"}
+                className="border rounded w-full min-h-[700px]"
+              />
+            </TabsContent>
 
 
 
-        </Tabs>
-      </TabsContent >
+          </Tabs>
+        </TabsContent >
 
-      <TabsContent value="tickets">
-        Aquí va el contenido de tickets
-      </TabsContent>
-    </Tabs >
+        <TabsContent value="tickets">
+          Aquí va el contenido de tickets
+        </TabsContent>
+      </Tabs >
+    </div>
   )
 }
